00925a05386bfdf64c173e16ba0f2547241da451
AlekaUaliyev <149511375+AlekaUaliyev@users.noreply.github.com>
2025-04-13 21:06:25 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from app import add, subtract
 
 class TestApp(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(3, 3), 5)
         self.assertEqual(add(-1, 1), 0)
 
     def test_subtract(self):
