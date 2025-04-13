f9c27083531f711e0f3991d894f2a808a3796929
AlekaUaliyev <149511375+AlekaUaliyev@users.noreply.github.com>
2025-04-13 21:32:56 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,7 +8,7 @@ class TestApp(unittest.TestCase):
 
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
-        self.assertEqual(subtract(0, 1), -1)
+        self.assertEqual(subtract(2, 1), -1)
 
 if __name__ == '__main__':
     unittest.main()
